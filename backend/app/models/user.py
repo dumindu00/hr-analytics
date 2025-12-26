155d8ebb8742411b80a635f5c92b159c863d7dbd
dumindu00 <duminduviraj26@gmail.com>
2025-12-26 11:52:56 +0530

    sql fix

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,14 +2,13 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database import Base
 
-
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    company_name = Column(String(100))
-    hashed_password = Column(String(225))
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    company_name = Column(String(100), nullable=False)
+    hashed_password = Column(String(225), nullable=False)
 
     monthly_performances = relationship(
         "MonthlyPerformance",
